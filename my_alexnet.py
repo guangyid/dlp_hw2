c6df9945919f37b544e4f433f801e766f7383c2c
guangyi <hanguangyi@mail.ustc.edu.cn>
2022-11-18 23:59:32 +0800

    done

---
+++ my_alexnet.py
@@ -0,0 +1,70 @@
+from typing import Any
+
+import torch
+import torch.nn as nn
+
+from torchvision._internally_replaced_utils import load_state_dict_from_url
+from torchvision.utils import _log_api_usage_once
+
+
+__all__ = ["AlexNet", "alexnet"]
+
+
+model_urls = {
+    "alexnet": "https://download.pytorch.org/models/alexnet-owt-7be5be79.pth",
+}
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.5) -> None:
+        super().__init__()
+        _log_api_usage_once(self)
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        #input [256,3,64,64]
+        x = self.features(x)#outputt[256,256,1,1]
+        x = self.avgpool(x)#output [256,256,6,6]把一个元素复制6*6遍？
+        x = torch.flatten(x, 1)#[256,9216]
+        x = self.classifier(x)#[256,200]
+        return x
+
+
+def alexnet(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> AlexNet:
+    r"""AlexNet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+    The required minimum input size of the model is 63x63.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    model = AlexNet(**kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls["alexnet"], progress=progress)
+        model.load_state_dict(state_dict)
+    return model
